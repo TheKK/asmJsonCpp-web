71a75f556147c98ea16de06efbf117d24f4304f3
yingrueil <yingrueil@synology.com>
2020-11-25 13:31:19 +0800

    [project] add docker.nix to produce docker image

---
+++ docker.nix
@@ -0,0 +1,21 @@
+{ obelisk ? import ./.obelisk/impl {
+  system = builtins.currentSystem;
+  iosSdkVersion = "10.2";
+} }:
+
+with obelisk.nixpkgs;
+
+let
+  project = import ./. { inherit obelisk; };
+  exe = project.exe;
+
+in dockerTools.buildLayeredImage {
+  name = "asmJsonCpp-ui";
+  tag = "latest";
+  contents = [ exe ];
+  config = {
+    WorkingDir = "${exe}";
+    Cmd = [ "${exe}/backend" "-p 8080" ];
+    ExposedPorts = { "8080" = { }; };
+  };
+}
