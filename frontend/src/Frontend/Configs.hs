6071a2a0178089b3244d0aaadd2699231ed19bd2
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2021-09-09 15:52:01 +0800

    [Frontend] strip after getAPIURL

--- frontend/src/Frontend/Configs.hs
+++ frontend/src/Frontend/Configs.hs
@@ -8,4 +8,4 @@ import qualified Data.Text.Encoding as T
 import Obelisk.Configs
 
 getAPIURL :: (HasConfigs m) => m T.Text
-getAPIURL = T.decodeUtf8 . fromMaybe "" <$> getConfig "frontend/api_url"
+getAPIURL = T.strip . T.decodeUtf8 . fromMaybe "" <$> getConfig "frontend/api_url"
